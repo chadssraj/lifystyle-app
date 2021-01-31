fd78eecbb9521c38a77944a0acdbfc0c3ef94ddf
unknown <chandragirisekhar43@gmail.com>
2021-01-31 09:51:35 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,14 @@ app.get("/", (req, res) => {
   res.send("server is ready ");
 });
 
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
   next();
